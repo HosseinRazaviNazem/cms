fb201e781189f42cb321c9afc2a3d9439a17b0cd
Sepehr <s.r.nazem@gmail.com>
2024-04-15 17:27:18 +0330

    order-product

--- database/migrations/2024_04_15_131230_create_profiles_table.php
+++ database/migrations/2024_04_15_131230_create_profiles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
 
-            $table->unsignedBigInteger('Customer_id');
+            $table->unsignedBigInteger('Customer_id')->onDelete('cascade');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('address')->nullable();
