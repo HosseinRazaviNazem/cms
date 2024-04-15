39c24b716eafac7822f787e2435ec0806241f00a
Sepehr <s.r.nazem@gmail.com>
2024-04-15 16:59:25 +0330

    add profile

---
+++ database/migrations/2024_04_15_131230_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('Customer_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('city');
+            $table->string('state')->after('city');
+            $table->boolean('profile_status')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
