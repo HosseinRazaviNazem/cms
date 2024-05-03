eca092708ad80b32cb3de87f0313aee1cf433cab
Amirnabipoor <Amirnp04@gmail.com>
2024-05-03 17:41:10 +0330

    add jwt v1

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,20 @@ Route::apiResource('customers/profile',\App\Http\Controllers\profile\ProfileCont
 //Route::post('customers/{customer_id}',[\App\Http\Controllers\profile\ProfileController::class, 'show']);
 //Route::post('customers/profiles/admin',[\App\Http\Controllers\profile\ProfileController::class, 'admin']);
 Route::apiResource('product',\App\Http\Controllers\product\ProductController::class);
+
+//Route::controller(\App\Http\Controllers\AuthController::class)->group(function () {
+//    Route::post('login', 'login');
+//    Route::post('register', 'register');
+//    Route::post('logout', 'logout');
+//    Route::post('refresh', 'refresh');
+//});
+
+    Route::group([ 'middleware' => 'auth:api'], function () {
+        Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+        Route::post('refresh', [\App\Http\Controllers\AuthController::class, 'refresh']);
+    });
+
+    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+
+
