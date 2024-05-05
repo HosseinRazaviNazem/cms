82c9a1ef4dedc05ab1eb7c7a62c4c012b6bfbb70
Amirnabipoor <Amirnp04@gmail.com>
2024-05-05 20:56:11 +0330

    v1

--- app/Http/Controllers/cart/CartController.php
+++ app/Http/Controllers/cart/CartController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\cart;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\cart\CartRequest;
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
-}
+    public function addItem(CartRequest $request)
+    {
+
+
+        $cartItem = Cart::create($request->validated());
+        $cartItem = CartItem::where('user_id', $request->customer_id)
+            ->where('product_id', $request->product_id)
+            ->first();
+
+        if ($cartItem) {
+            // Update quantity
+            $cartItem->quantity += $request->quantity;
+            $cartItem->save();
+        } else {
+            // Add new item to cart
+            $cartItem = new CartItem();
+            $cartItem->user_id = $request->user_id;
+            $cartItem->product_id = $request->product_id;
+            $cartItem->quantity = $request->quantity;
+            $cartItem->save();
+        }
+
+        return response()->json(['message' => 'Item added to cart successfully']);
+    }
