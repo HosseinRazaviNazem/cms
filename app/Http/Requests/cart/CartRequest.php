9581c9ba06f19a770009f43a68f965a5a254e183
Amirnabipoor <Amirnp04@gmail.com>
2024-05-05 20:33:36 +0330

    hello

--- app/Http/Requests/cart/CartRequest.php
+++ app/Http/Requests/cart/CartRequest.php
@@ -9,10 +9,10 @@ class CartRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
-    }
+//    public function authorize(): bool
+//    {
+//        return false;
+//    }
 
     /**
      * Get the validation rules that apply to the request.
@@ -22,7 +22,10 @@ class CartRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'quantity' => 'required|integer|min:1',
+            'customer_id' => 'required',
+            'product_id' => 'required',
+
         ];
     }
 }
