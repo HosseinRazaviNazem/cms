f57c9b6c7c1a49fb7e390d772bef88841f3af4cd
Amirnabipoor <Amirnp04@gmail.com>
2024-05-25 13:45:14 +0330

    v2

---
+++ app/Http/Resources/admin/AdminCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AdminCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'message' => 'all Admins',
+            'data' => $this->collection,
+        ];
+    }
+}
