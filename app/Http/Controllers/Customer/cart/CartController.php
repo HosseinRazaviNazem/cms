77e00043a7c1d70a2a0cfd44951e82c84a8869a8
Amirnabipoor <Amirnp04@gmail.com>
2024-05-28 17:54:41 +0330

    ok

--- app/Http/Controllers/Customer/cart/CartController.php
+++ app/Http/Controllers/Customer/cart/CartController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Customer\cart;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Customer\cart\CartRequest;
-use App\Http\Resources\cart\CartResource;
+use App\Http\Resources\Customer\cart\CartResource;
 use App\Models\Cart;
 
 class CartController extends Controller
