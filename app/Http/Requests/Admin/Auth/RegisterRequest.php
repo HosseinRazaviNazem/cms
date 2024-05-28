50a9456425813400f97412db17c61bfb8e820f2e
Amirnabipoor <Amirnp04@gmail.com>
2024-05-28 14:41:59 +0330

    new

---
+++ app/Http/Requests/Admin/Auth/RegisterRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\Admin\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'phone' => 'required|string|unique:customers,phone|max:12',
+            'email' => 'required|email|unique:customers|max:255|email',
+            'username' => 'required|string|unique:customers,username|max:50',
+            'password' => 'required|string|min:6',
+        ];
+    }
+}
